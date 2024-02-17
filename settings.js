99a2dd82a735f3a59eaf206179ac5df754c1ab78
Jupiterke <118110048+Jupiterke@users.noreply.github.com>
2024-02-17 11:03:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['919968778724']
-global.premium = ['919968778724']
-global.ownernomer = '919968778724'
+global.owner = ['254756574562']
+global.premium = ['254756574562']
+global.ownernomer = '254756574562'
 global.ownername = '𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹'
 global.botname = '𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹 𝑺𝑬𝑹 𝑩𝑶𝑻'
 global.caption = 'ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ'
